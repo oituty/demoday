8d97e74f768a632dd5641ccf26e841b31df07c2b
Tayna Alves <taynaalro@gmail.com>
2018-02-04 01:16:08 -0200

    animacaoquaseok

--- home.js
+++ home.js
@@ -11,5 +11,18 @@ let h4 = document.querySelectorAll('.subtituloinicio');
     setInterval(avancarSlide, 3500);
     avancarSlide();
 
-  
 
+// let frase = '......';
+//     let ponto = document.querySelector('.pontos');
+//     let contador = 0;
+//     function exibirLetra(){
+//       let letra = frase[contador];
+//       ponto.innerHTML += letra;
+//       contador++;
+//       if(contador >= frase.length){
+//         clearInterval(intervalo);
+//       } 
+//     }
+//     let intervalo = setInterval(exibirLetra, 400);
+
+    
