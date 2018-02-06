bca0dfcc44edf5a7d21255f1d4a93fb78dd1963e
Tayna Alves <taynaalro@gmail.com>
2018-02-06 02:13:35 -0200

    progresso

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+// SLIDE DE FRASES INICIO
+
 let carro1 = document.querySelectorAll('.subtituloinicio');
   let slider = 0;
     function avancarSlide1(){
@@ -11,22 +13,9 @@ let carro1 = document.querySelectorAll('.subtituloinicio');
     setInterval(avancarSlide1, 3500);
     avancarSlide1();
 
+// LETRAS DIGITANDO ESPAÇO ONGS
 
-// let carro2 = document.querySelectorAll('.carro2');
-//     let slider = 0;
-//       function avancarSlide2(){
-//         carro2[slider].style.display = 'none';
-//           slider++;
-//       if(slider >= carro2.length){
-//                 slider = 0;
-//       }
-//         carro2[slider].style.display = 'block';
-//       }
-//       setInterval(avancarSlide2, 3500);
-//       avancarSlide2();
-
-
-let frase1 = 'Um projeto criado para ajestáSei lá  ajudando o mundo. Sei lá criado para estáajudando o mundo. Sei lá.projeto criasasa  asasa doparuem já estáajudando o mundo. Sei unto.';
+let frase1 = 'Um projeto criado para ajestáSdo. Um projeto criado para ajestáSdo Um projeto criado para ajestáSdo';
       let f1 = document.querySelector('.teste');
       let contador = 0;
       function exibirLetra(){
@@ -35,12 +24,28 @@ let frase1 = 'Um projeto criado para ajestáSei lá  ajudando o mundo. Sei lá c
         contador++;
         if(contador >= frase1.length){
           clearInterval(intervalo);
+          
+          // f1.innerHTML = '';
+          // contador = 0;
+        
         }
+
       }
       let intervalo = setInterval(exibirLetra, 130);
+      // clearInterval(intervalo);
 
-// $('.carousel').carousel({
-//   interval: 3000
-// })
-      
+// CONTROLE DE TIME CARROSSEL ONGS
+
+$('.carousel').carousel({
+  interval: 3500
+});
+
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() < 300) {
+//       $('.navbar').css("background-color", "#red")
+//   } else {
+//       $('.navbar').css("background-color", "brown")
+//   }
+// });      
+
 
